9f152f0afd24d0b16c4fb2b7965bceab45bfa68b
Saransh Singh <saranshsingh2205@gmail.com>
2025-11-20 00:32:56 -0800

    commit 7

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,10 +5,12 @@ module.exports = [
     name: "strapi::cors",
     config: {
       origin: [
-        "http://localhost:3000",
-        "https://clinic-snowy-five.vercel.app",
-        /^https:\/\/clinic-snowy-five-.*\.vercel\.app$/,
-      ],
+          "http://localhost:3000",
+          "https://clinic-snowy-five.vercel.app",
+          "https://clinicbackend-production-61e1.up.railway.app",
+          /^https:\/\/clinic-snowy-five-.*\.vercel\.app$/,
+        ],
+
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
       headers: ["Content-Type", "Authorization", "Origin", "Accept"],
       keepHeadersOnError: true,
