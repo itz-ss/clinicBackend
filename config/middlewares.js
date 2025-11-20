45f61abceb4bc59bf571bb4db1c9b011ad054bdb
Saransh Singh <saranshsingh2205@gmail.com>
2025-11-20 02:26:44 -0800

    commit 15

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,20 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: false,
+      headers: ['*'],
+      origin: [
+        'http://localhost:3000',
+        'https://clinic-snowy-five.vercel.app',   // frontend
+        'https://clinicbackend-production-61e1.up.railway.app', // backend itself
+      ],
+      methods: ['GET', 'HEAD'], // frontend only views data
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::security',
   'strapi::poweredBy',
   'strapi::query',
