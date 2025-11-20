7269c904c51b4d05feef1e45aa66e0a2a02f62e1
Saransh Singh <saranshsingh2205@gmail.com>
2025-11-20 01:10:00 -0800

    commit 9

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,26 +1,25 @@
 module.exports = [
-  "strapi::logger",
-  "strapi::errors",
+  'strapi::logger',
+  'strapi::errors',
   {
-    name: "strapi::cors",
+    name: 'strapi::cors',
     config: {
+      enabled: true,
+      headers: ['*'],
       origin: [
-          "http://localhost:3000",
-          "https://clinic-snowy-five.vercel.app",
-          "https://clinicbackend-production-61e1.up.railway.app",
-          /^https:\/\/clinic-snowy-five-.*\.vercel\.app$/,
-        ],
-
-      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
-      keepHeadersOnError: true,
+        'http://localhost:3000',
+        'https://clinic-snowy-five.vercel.app',   // frontend
+        'https://clinicbackend-production-61e1.up.railway.app', // backend itself
+      ],
+      methods: ['GET', 'HEAD'], // frontend only views data
+      keepHeaderOnError: true,
     },
   },
-  "strapi::security",
-  "strapi::poweredBy",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::security',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
